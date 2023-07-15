555c82ea14b4f5286937a846bf34a227354f1992
nriosvillaco <nicholasrios1213@gmail.com>
2023-07-15 18:36:23 -0400

    extract number of reviews

--- code/single_recipe/tasteofhome_pie.R
+++ code/single_recipe/tasteofhome_pie.R
@@ -1,6 +1,7 @@
 library(rvest)
 library(xml2)
 library(here)
+library(stringr)
 
 #load html from website
 recipe_url <- "https://www.tasteofhome.com/recipes/easy-fresh-strawberry-pie/"
@@ -9,4 +10,9 @@ page <- read_html(recipe_url)
 #extract recipe name
 recipe_name <- page %>%
   html_node("title") %>%
+  html_text()
+
+#extract number of reviews
+recipe_review_count <- page %>%
+  html_node("div.rating a.recipe-comments-scroll") %>%
   html_text()
