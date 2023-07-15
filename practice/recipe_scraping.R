f695fd012660178517396d06081cb31f4f635eeb
nriosvillaco <nicholasrios1213@gmail.com>
2023-07-15 17:20:53 -0400

    extract recipe name

--- practice/recipe_scraping.R
+++ practice/recipe_scraping.R
@@ -4,4 +4,11 @@ library(here)
 
 #find practice file path
 path <- here("practice", "practice_recipe.html")
-raw_html <- read_html(path)
+recipe_page <- read_html(path)
+
+#extract recipe name
+recipe_name <- recipe_page %>%
+  html_node("title") %>%
+  html_text()
+
+#extract recipe popularity
